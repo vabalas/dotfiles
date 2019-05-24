250d7574819014868d5d4d9a2ef41fce26d73fe1
vabalas <lukasvabalas@gmail.com>
2019-05-24 17:21:51 +0300

    Change emacs font size.

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -42,5 +42,5 @@ There are two things you can do about this warning:
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Fira Mono" :foundry "CTDB" :slant normal :weight normal :height 158 :width normal))))
+ '(default ((t (:family "Fira Mono" :foundry "CTDB" :slant normal :weight normal :height 180 :width normal))))
  '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 2.0)))))
